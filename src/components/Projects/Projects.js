fef9b9a114da1976c20553624af41ae36b74bb06
Farish3444 <farish.mechanical@gmail.com>
2022-10-10 21:31:06 +0530

    resume jpeg

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,6 +11,7 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import Billing from '../../Assets/Projects/Billingimage.jpg';
 
 function Projects() {
   return (
@@ -50,10 +51,10 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={sport}
+              imgPath={Billing}
               isBlog={false}
-              title="PlayHere"
-              description={textpara.PlayHere}
+              title="ElizzElite"
+              description={textpara.ElizzElite}
             />
           </Col>
 
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import resumeimg from './RESUME_2022_page-0001.jpg';  
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
@@ -10,16 +11,24 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 // import  Resume  from '../../Assets/../Assets/RESUME_2022.pdf';
 
-const resumeLink = 
-// "https://github.com/Farish3444/Farish_Portfolio/blob/main/src/Assets/RESUME_2022.pdf"
-  // "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf"
-  "https://raw.githubusercontent.com/Farish3444/Farish_Portfolio/blob/main/src/Assets/RESUME_2022.pdf";
+// const resumeLink = 
+// // "https://github.com/Farish3444/Farish_Portfolio/blob/main/src/Assets/RESUME_2022.pdf"
+//   // "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf"
+//   "https://raw.githubusercontent.com/Farish3444/Farish_Portfolio/blob/main/src/Assets/RESUME_2022.pdf";
 
 
 
 function ResumeNew() {
   
   const [width, setWidth] = useState(1200);
+
+  const[numpage,setNumpages] = useState(null);
+  const[pageNumber,setPageNumber] = useState(1);
+
+  const onDocumentLoadSuccess =({numpages})=>{
+    setNumpages(numpage);
+    setPageNumber(1);
+  }
 
   useEffect(() => {
     setWidth(window.innerWidth)
@@ -29,35 +38,24 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button> 
-         </Row>
-
-        <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+        {/* <Row className="resume">
+          <Document file='/RESUME_2022.pdf' onLoadSuccess={onDocumentLoadSuccess}  className="d-flex justify-content-center" >
+            <Page 
+            height='600' 
+            pageNumber={pageNumber} 
+            scale={width > 786 ? 1.7 : 0.6}
+             />
           </Document>
-        </Row>
+        </Row> */}
 
+        <img 
+        src={resumeimg}
+        alt='resumeImg'
+        style={{maxWidth:'50%'}}
+        />
+<br />  
         <Row style={{ justifyContent: "center", position: "relative" }}>
-           <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button> 
-        <a className="buttonInsta">Download CV here</a>  
+        <a className="buttonInsta">Download CV heres</a>  
         </Row>
       </Container>
     </div>
