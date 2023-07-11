1c86b458be19136700cc470267e47f3140071aa2
farishikomet <farish.j@ikomet.com>
2023-07-11 13:39:07 +0530

    resume 2023 added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -75,21 +75,6 @@ function Projects() {
               description={textpara.applypan}
             />
           </Col>
-
-          
-
-          
-
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-            />
-          </Col>  */}
         </Row>
       </Container>
     </Container>
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import resumeimg from './RESUME_2022_page-0001.jpg';  
+import resumeimg from './FarishResume2023_page-0001.jpg';  
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
@@ -11,10 +11,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 // import  Resume  from '../../Assets/../Assets/RESUME_2022.pdf';
 
-// const resumeLink = 
-// // "https://github.com/Farish3444/Farish_Portfolio/blob/main/src/Assets/RESUME_2022.pdf"
-//   // "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf"
-//   "https://raw.githubusercontent.com/Farish3444/Farish_Portfolio/blob/main/src/Assets/RESUME_2022.pdf";
+
 
 
 
@@ -34,28 +31,28 @@ function ResumeNew() {
     setWidth(window.innerWidth)
   }, []);
 
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = resumeimg;
+    link.download = "FarishResume2023_page-0001.jpg";
+    link.click();
+  };
+
   return (
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        {/* <Row className="resume">
-          <Document file='/RESUME_2022.pdf' onLoadSuccess={onDocumentLoadSuccess}  className="d-flex justify-content-center" >
-            <Page 
-            height='600' 
-            pageNumber={pageNumber} 
-            scale={width > 786 ? 1.7 : 0.6}
-             />
-          </Document>
-        </Row> */}
-
+      
         <img 
         src={resumeimg}
         alt='resumeImg'
         style={{maxWidth:'50%'}}
         />
 <br />  
+<br />
+<br />
         <Row style={{ justifyContent: "center", position: "relative" }}>
-        <a className="buttonInsta">Download CV heres</a>  
+        <a className="buttonInsta" onClick={handleDownload}>Download CV heres</a>  
         </Row>
       </Container>
     </div>
